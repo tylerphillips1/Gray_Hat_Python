0a91f86aca9edff66491fefee873b78de8488bee
Tyler Phillips <tylerphillips1@u.boisestate.edu>
2017-10-04 11:53:20 -0600

    Update my_test.py

--- chapter_03/1_Get_PID/my_test.py
+++ chapter_03/1_Get_PID/my_test.py
@@ -1,7 +1,7 @@
 import my_debugger 
 
+# Launch the debugger
 debugger = my_debugger.debugger()
 
+# Using Windows API to laod given .exe file
 debugger.load("c:\\windows\\system32\\calc.exe")
-
-# See if this works
