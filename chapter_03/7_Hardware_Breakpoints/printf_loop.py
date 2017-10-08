e908fefc7f73ee82c636109e6c9a4564a96828de
Vasanth Geethan Raju <vasanthgeethan.raju@gmail.com>
2017-10-08 15:35:58 -0600

    Update printf_loop.py

--- chapter_03/7_Hardware_Breakpoints/printf_loop.py
+++ chapter_03/7_Hardware_Breakpoints/printf_loop.py
@@ -1,11 +1,12 @@
 from ctypes import *
 import time
 
+#msvcrt - Microsoft C Runtime Library
 msvcrt = cdll.msvcrt
 
 counter=0
 
 while 1:
-    msvcrt.printf("Loop iteration %d!\n" % counter)
-    time.sleep(2)
+    msvcrt.printf("Loop iteration %d!\n",counter) #ctypes to print
+    time.sleep(2) #Suspend execution of the current thread for 2 seconds
     counter +=1
