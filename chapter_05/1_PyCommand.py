3b514380080c2ec7b7c420d92ff292f2a176ac1c
Tyler Phillips <tylerphillips1@u.boisestate.edu>
2017-10-11 13:42:46 -0600

    Added chapter 5 files

---
+++ chapter_05/1_PyCommand.py
@@ -0,0 +1,10 @@
+from immlib import *
+
+def main(args): 
+    # instantiate a immlib.Debugger instance
+    imm= Debugger()
+    
+    return "[*] Pycommand Executed!"
+    
+    
+    
---
+++ chapter_05/2_findinstruction.py
@@ -0,0 +1,13 @@
+from immlib import *
+
+def main(args):
+    imm = Debugger()
+    search_code = " ".join(args)
+    search_bytes = imm.assemble(search_code)
+    search_results = imm.search(search_bytes)
+    for hit in search_results:
+        code_page = imm.getMemoryPageByAddress(hit)  
+        access = code_page.getAccess(human = True)
+        if "execute" in access.lower():
+            imm.log("[*] Found: %s (0x%08x)" %( search_code, hit ), address = hit)
+    return "[*] Finished searching for instructions, check the Log window."
